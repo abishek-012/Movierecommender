58a1123aa38caccdab73e214ef33e343625ea546
Abishek Kandan <abishekkandan05@gmail.com>
2026-02-13 11:11:08 +0530

    Stopwords commit

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,20 @@ import os
 
 OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
 
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords", download_dir=nltk_data_path)
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt", download_dir=nltk_data_path)
 
 # ===============================
 # 2️⃣ LOAD DATA + BUILD MODEL
